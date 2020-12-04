0ca42c072c25b70cb2623afa4e13069a1833ca6a
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2020-12-04 22:21:12 +0800

    build login success screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/screens/forgot_password/forgot_password_screen.dart';
+import 'package:ecommerce_app/screens/login_success/login_success_screen.dart';
 import 'package:ecommerce_app/screens/sign_in/sign_in_screen.dart';
 import 'package:flutter/widgets.dart';
 import 'package:ecommerce_app/screens/splash/splash_screen.dart';
@@ -7,4 +8,5 @@ final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
   SignInScreen.routeName: (context) => SignInScreen(),
   ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+  LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
 };
