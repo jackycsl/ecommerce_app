2c047b4858290cf57666efaae8d8039b2e7337db
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2020-12-05 15:39:32 +0800

    refactor form error validation

--- lib/screens/forgot_password/components/body.dart
+++ lib/screens/forgot_password/components/body.dart
@@ -1,11 +1,6 @@
-import 'package:ecommerce_app/components/custom_svg_icon.dart';
-import 'package:ecommerce_app/components/default_button.dart';
-import 'package:ecommerce_app/components/form_error.dart';
-import 'package:ecommerce_app/components/no_account_text.dart';
+import 'package:ecommerce_app/screens/forgot_password/components/forgot_password_form.dart';
 import 'package:ecommerce_app/size_config.dart';
 import 'package:flutter/material.dart';
-
-import '../../../constants.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -35,76 +30,6 @@ class Body extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class ForgotPassForm extends StatefulWidget {
-  @override
-  _ForgotPassFormState createState() => _ForgotPassFormState();
-}
-
-class _ForgotPassFormState extends State<ForgotPassForm> {
-  final _formKey = GlobalKey<FormState>();
-  List<String> errors = [];
-  String email;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onSaved: (newValue) => email = newValue,
-            onChanged: (value) {
-              if (value.isNotEmpty && errors.contains(kEmailNullError)) {
-                setState(() {
-                  errors.remove(kEmailNullError);
-                });
-              } else if (emailValidatorRegExp.hasMatch(value) &&
-                  errors.contains(kInvalidEmailError)) {
-                setState(() {
-                  errors.remove(kInvalidEmailError);
-                });
-              }
-              return null;
-            },
-            validator: (value) {
-              if (value.isEmpty && !errors.contains(kEmailNullError)) {
-                setState(() {
-                  errors.add(kEmailNullError);
-                });
-              } else if (!emailValidatorRegExp.hasMatch(value) &&
-                  !errors.contains(kInvalidEmailError)) {
-                setState(() {
-                  errors.add(kInvalidEmailError);
-                });
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              labelText: "Email",
-              hintText: "Enter your email",
-              suffixIcon: CustomSvgIcon(
-                svgIcon: "assets/icons/Mail.svg",
-              ),
-            ),
-          ),
-          SizedBox(height: getProportionateScreenHeight(30)),
-          FormError(errors: errors),
-          SizedBox(height: SizeConfig.screenHeight * 0.1),
-          DefaultButton(
-            text: "Continue",
-            press: () {
-              if (_formKey.currentState.validate()) {}
-            },
-          ),
-          SizedBox(height: SizeConfig.screenHeight * 0.1),
-          NoAccountText(),
-        ],
       ),
     );
   }
---
+++ lib/screens/forgot_password/components/forgot_password_form.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:ecommerce_app/components/custom_svg_icon.dart';
+import 'package:ecommerce_app/components/default_button.dart';
+import 'package:ecommerce_app/components/form_error.dart';
+import 'package:ecommerce_app/components/no_account_text.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class ForgotPassForm extends StatefulWidget {
+  @override
+  _ForgotPassFormState createState() => _ForgotPassFormState();
+}
+
+class _ForgotPassFormState extends State<ForgotPassForm> {
+  final _formKey = GlobalKey<FormState>();
+  List<String> errors = [];
+  String email;
+
+  void addError({String error}) {
+    if (!errors.contains(error))
+      setState(() {
+        errors.add(error);
+      });
+  }
+
+  void removeError({String error}) {
+    if (errors.contains(error))
+      setState(() {
+        errors.remove(error);
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            onSaved: (newValue) => email = newValue,
+            onChanged: (value) {
+              if (value.isNotEmpty) {
+                removeError(error: kEmailNullError);
+              } else if (emailValidatorRegExp.hasMatch(value)) {
+                removeError(error: kInvalidEmailError);
+              }
+              return null;
+            },
+            validator: (value) {
+              if (value.isEmpty) {
+                addError(error: kEmailNullError);
+                return "";
+              } else if (!emailValidatorRegExp.hasMatch(value)) {
+                addError(error: kInvalidEmailError);
+                return "";
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              labelText: "Email",
+              hintText: "Enter your email",
+              suffixIcon: CustomSvgIcon(
+                svgIcon: "assets/icons/Mail.svg",
+              ),
+            ),
+          ),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          FormError(errors: errors),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          DefaultButton(
+            text: "Continue",
+            press: () {
+              if (_formKey.currentState.validate()) {}
+            },
+          ),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          NoAccountText(),
+        ],
+      ),
+    );
+  }
+}
