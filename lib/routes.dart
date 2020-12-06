bc2858b83cbc0fe406d304b3cb8aaace6eaa4944
jackychan@virtuosity-global.com <jackychan@virtuosity-global.com>
2020-12-06 18:46:26 +0800

    build dismissable cart screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/screens/cart/cart_screen.dart';
 import 'package:ecommerce_app/screens/complete_profile/complete_profile_screen.dart';
 import 'package:ecommerce_app/screens/details/details_screen.dart';
 import 'package:ecommerce_app/screens/forgot_password/forgot_password_screen.dart';
@@ -19,4 +20,5 @@ final Map<String, WidgetBuilder> routes = {
   OtpScreen.routeName: (context) => OtpScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
   DetailsScreen.routeName: (context) => DetailsScreen(),
+  CartScreen.routeName: (context) => CartScreen(),
 };
